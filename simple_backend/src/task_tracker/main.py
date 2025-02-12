e7c6c65708d06da7d8fa9d12242a936f9cd890e1
Lev Patskevich <lion.patskevich@yandex.ru>
2025-02-12 13:49:42 +0300

    fix: add database, add requirements.txt

--- simple_backend/src/task_tracker/main.py
+++ simple_backend/src/task_tracker/main.py
@@ -1,14 +1,7 @@
 from fastapi import FastAPI, Body
-from pydantic import BaseModel
+from json_db_lite import JSONDatabase
 
-
-class Task(BaseModel):
-    id: int
-    text: str = "Description"
-    status: bool = False
-
-
-tasks_db = []
+db_client = JSONDatabase("db.json")
 
 
 app = FastAPI()
@@ -16,34 +9,38 @@ app = FastAPI()
 
 @app.get("/tasks")
 def get_tasks() -> list | str:
-    if tasks_db:
-        return tasks_db
+    stat = db_client.get_all_records()
+    if stat:
+        return stat
     return "Tasks list is empty!"
 
 
 @app.post("/tasks")
-def create_task(task: Task) -> str:
-    if tasks_db:
-        task.id = max(tasks_db, key=lambda x: x.id).id + 1
+def create_task(text: str = Body(), status: bool = Body()) -> str:
+    stat = db_client.get_all_records()
+    if db_client:
+        task_id = len(stat) + 1
     else:
-        task.id = 0
-    tasks_db.append(task)
+        task_id = 1
+    db_client.add_records({"id": task_id, "text": text, "status": status})
     return "Task created!"
 
 
 @app.put("/tasks/{task_id}")
-def update_task(task_id: int, message: str = Body(), status: bool = Body()) -> str:
-    if task_id <= len(tasks_db):
-        changeable_task = tasks_db[task_id]
-        changeable_task.text = message
-        changeable_task.status = status
+def update_task(task_id: int, text: str = Body(), status: bool = Body()) -> str:
+    stat = db_client.get_all_records()
+    if task_id <= len(stat):
+        db_client.update_record_by_key(
+            upd_filter={"id": task_id}, new_data=[{"text": text, "status": status}]
+        )
         return "Task changed!"
     return f"Task ID{task_id} is not in task list!"
 
 
 @app.delete("/tasks/{task_id}")
 def delete_task(task_id: int) -> str:
-    if task_id <= len(tasks_db):
-        tasks_db.pop(task_id)
+    stat = db_client.get_all_records()
+    if task_id <= len(stat):
+        db_client.delete_record_by_key(key="id", value=task_id)
         return f"Task ID{task_id} deleted!"
     return f"Task ID{task_id} is not in task list!"
