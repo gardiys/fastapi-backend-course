1d1663ea8e0ddd9a156b10117dd36ca77d3d0455
Ilyas <106924287+IlyasYusupov@users.noreply.github.com>
2025-02-21 02:11:29 +0300

    сделал 3 задание

---
+++ simple_backend/src/task_tracker/cloudflare.py
@@ -0,0 +1,31 @@
+import requests
+import json
+import yaml
+
+
+with open("settings.yml", "r", encoding="utf-8") as file:
+    settings = yaml.safe_load(file)
+
+
+class CloudFlare:
+    def __init__(self):
+        self.__cloudflare_id = settings.get("cloudflare_id")
+        self.__cloudflare_token = settings.get("cloudflare_token")
+        self.__cloudflare_url = f"https://api.cloudflare.com/client/v4/accounts/{self.__cloudflare_id}/ai/run/@cf/meta/llama-3-8b-instruct"
+
+    @property
+    def headers(self):
+        return {"Authorization": f"Bearer {self.__cloudflare_token}"}
+
+    def send_prompt(self, task_name):
+        try:
+            data = {"prompt": f"Объясни решение этой задачи на русском языке: {task_name}"}
+
+            response = requests.post(
+                url=self.__cloudflare_url, headers=self.headers, json=data
+            )
+            print(response.json()["result"]["response"])
+            return response.json()["result"]["response"]
+
+        except Exception as e:
+            return {"error": str(e)}
--- simple_backend/src/task_tracker/models.py
+++ simple_backend/src/task_tracker/models.py
@@ -1,13 +1,18 @@
 from stateless import Stateless
+from cloudflare import CloudFlare
+
+
+state = Stateless()
+llm = CloudFlare()
 
 
 # При создании задачи указывается ее имя, id - автоинкремент, status - Открыта (так как только создана)
 class MyTasks:
     def __init__(self, name):
-        self.__state = Stateless()
         self.__task_id = self.__initial_task_id()
         self.__task_name = name
         self.__status = "Открыта"
+        self.solution = llm.send_prompt(name)
 
     @property
     def task_id(self):
@@ -31,12 +36,29 @@ class MyTasks:
         self.__status = status
 
     @property
+    def solution(self):
+        return self.__solution
+
+    @solution.setter
+    def solution(self, text):
+        if "error" in text:
+            print(text)
+            self.__solution = "-"
+        else:
+            self.__solution = text
+
+    @property
     def info(self):
-        return {"id": self.task_id, "name": self.task_name, "status": self.status}
+        return {
+            "id": self.task_id,
+            "name": self.task_name,
+            "status": self.status,
+            "solution": self.solution,
+        }
 
     # функция для автоматической записи id задачи (id не повторяющийся)
     def __initial_task_id(self):
-        if not (tasks := self.__state.get_state()):
+        if not (tasks := state.get_state()):
             return 1
 
         return tasks[-1].get("id") + 1
