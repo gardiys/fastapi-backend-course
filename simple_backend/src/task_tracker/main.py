ab66c9ff89c58f6a4696e4b1778163a6ece529d3
shara-code <shara666.r@gmail.com>
2025-02-23 13:21:19 +0300

    updated main.py added tasks.json(storage in a JSON file)

--- simple_backend/src/task_tracker/main.py
+++ simple_backend/src/task_tracker/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI, HTTPException
+import json
 
 app = FastAPI()
 
-tasks = []
-task_id_counter = 1
+TASK_FILE = "tasks.json"
 
 
 class Task:
@@ -13,12 +13,47 @@ class Task:
         self.status = status
 
 
+def load_tasks():
+    try:
+        with open(TASK_FILE, "r") as f:
+            tasks_data = json.load(f)
+            tasks = []
+            global task_id_counter
+            max_id = 0
+            for task_data in tasks_data:
+                task = Task(
+                    id=task_data["id"],
+                    title=task_data["title"],
+                    status=task_data["status"],
+                )
+                tasks.append(task)
+                if task.id > max_id:
+                    max_id = task.id
+            task_id_counter = max_id + 1
+            return tasks
+    except FileNotFoundError:
+        return []
+
+
+def save_tasks(tasks_to_save):
+    with open(TASK_FILE, "w") as f:
+        tasks_list_for_json = []
+        for task in tasks_to_save:
+            tasks_list_for_json.append(
+                {"id": task.id, "title": task.title, "status": task.status}
+            )
+        json.dump(tasks_list_for_json, f, indent=4)
+
+
+tasks = load_tasks()
+task_id_counter = 1 if not tasks else max(task.id for task in tasks) + 1
+
+
 @app.get("/tasks")
 async def get_tasks():
-    task_list = []
-    for task in tasks:
-        task_list.append({"id": task.id, "title": task.title, "status": task.status})
-    return task_list
+    return [
+        {"id": task.id, "title": task.title, "status": task.status} for task in tasks
+    ]
 
 
 @app.post("/tasks")
@@ -26,6 +61,7 @@ async def create_task(task_data: dict):
     global task_id_counter
     new_task = Task(id=task_id_counter, title=task_data["title"])
     tasks.append(new_task)
+    save_tasks(tasks)
     task_id_counter += 1
     return {"id": new_task.id, "title": new_task.title, "status": new_task.status}
 
@@ -38,6 +74,7 @@ async def update_task(task_id: int, task_data: dict):
                 task.title = task_data["title"]
             if "status" in task_data:
                 task.status = task_data["status"]
+            save_tasks(tasks)
             return {"id": task.id, "title": task.title, "status": task.status}
     raise HTTPException(status_code=404, detail="Task not found")
 
@@ -49,4 +86,5 @@ async def delete_task(task_id: int):
     tasks = [task for task in tasks if task.id != task_id]
     if len(tasks) == original_tasks_count:
         raise HTTPException(status_code=404, detail="Task not found")
+    save_tasks(tasks)
     return {"message": "Task deleted"}
