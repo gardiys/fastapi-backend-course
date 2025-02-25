ee8e8897206bc6e27a9e397ea4971d68603d1cb6
Maksim2801 <whellwood@gmail.com>
2025-02-25 22:30:56 +0300

    add logic api

--- simple_backend/src/task_tracker/main.py
+++ simple_backend/src/task_tracker/main.py
@@ -2,18 +2,27 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+tasks = []
+
 @app.get("/tasks")
 def get_tasks():
-    pass
+    return tasks
 
 @app.post("/tasks")
-def create_task(task):
-    pass
+def create_task(id, name, status):
+    a = []
+    a.append(id)
+    a.append(name)
+    a.append(status)
+    tasks.append(a)
+    return 'Task created'
 
 @app.put("/tasks/{task_id}")
 def update_task(task_id: int):
-    pass
+    tasks[task_id] = 'Updated'
+    return 'Task updated'
 
 @app.delete("/tasks/{task_id}")
 def delete_task(task_id: int):
-    pass
+    del tasks[task_id]
+    return 'Task deleted'
