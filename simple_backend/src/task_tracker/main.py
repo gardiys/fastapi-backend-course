892ce26be54c7ab9e4ea2d613ae75782eb133f71
Maksim2801 <whellwood@gmail.com>
2025-02-26 11:24:05 +0300

    Создание логики запросов

--- simple_backend/src/task_tracker/main.py
+++ simple_backend/src/task_tracker/main.py
@@ -2,27 +2,34 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-tasks = []
+class Task():
+    id: int
+    name: str
+    status: str
+
+tasks = [
+    Task(id=1, name='Sample Task', status='Open')
+]
 
 @app.get("/tasks")
 def get_tasks():
     return tasks
 
 @app.post("/tasks")
-def create_task(id, name, status):
-    a = []
-    a.append(id)
-    a.append(name)
-    a.append(status)
-    tasks.append(a)
-    return 'Task created'
+def create_task(task: Task):
+    new_task = Task(id=task.id, name=task.name, status=task.status)
+    tasks.append(new_task)
+    return new_task
 
 @app.put("/tasks/{task_id}")
-def update_task(task_id: int):
-    tasks[task_id] = 'Updated'
-    return 'Task updated'
+def update_task(task_id: int, task: Task):
+    for i, t in enumerate(tasks):
+        if t.id == task_id:
+            tasks[i] = Task(id=task.id, name=task.name, status=task.status)
+            return tasks[i]
 
 @app.delete("/tasks/{task_id}")
 def delete_task(task_id: int):
-    del tasks[task_id]
-    return 'Task deleted'
+    global tasks
+    tasks = [t for t in tasks if t.id != task_id]
+    return
