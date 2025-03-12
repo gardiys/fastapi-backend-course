8ea0609e295cd8516e3392747584a3d399392462
Lev Patskevich <lion.patskevich@yandex.ru>
2025-03-12 18:31:15 +0300

    fix: change DataBase class. Add "try/except" block for methods.

--- simple_backend/src/task_tracker/main.py
+++ simple_backend/src/task_tracker/main.py
@@ -3,7 +3,8 @@ from json_db_lite import JSONDatabase
 
 
 class DataBase:
-    db_client = JSONDatabase("db.json")
+    def __init__(self, db_client):
+        self.db_client = db_client
 
     @property
     def get_task(self) -> str | list:
@@ -13,35 +14,65 @@ class DataBase:
         return "Tasks list is empty!"
 
     def create_task(self, message: str, status: bool):
-        stat = self.db_client.get_all_records()
-        if stat:
-            task_id = len(stat) + 1
-        else:
-            task_id = 1
-        self.db_client.add_records({"id": task_id, "text": message, "status": status})
-        return "Task created!"
+        try:
+            stat = self.db_client.get_all_records()
+            if stat:
+                task_id = len(stat) + 1
+            else:
+                task_id = 1
+            self.db_client.add_records({"id": task_id, "text": message, "status": status})
+            return {
+                "success": True,
+                "data": {
+                    "id": task_id,
+                    "text": message,
+                    "status": status
+                }
+            }
+        except Exception as e:
+            return {
+                "success": False,
+                "error": str(e)
+            }
 
     def update_task(self, task_id: int, message: str, status: bool):
-        stat = self.db_client.get_all_records()
-        if task_id <= len(stat):
-            self.db_client.update_record_by_key(
-                upd_filter={"id": task_id},
-                new_data=[{"text": message, "status": status}],
-            )
-            return "Task changed!"
-        return f"Task ID{task_id} is not in task list!"
+        try:
+            stat = self.db_client.get_all_records()
+            if task_id <= len(stat):
+                self.db_client.update_record_by_key(
+                    upd_filter={"id": task_id},
+                    new_data=[{"text": message, "status": status}],
+                )
+                return {
+                    "success": True,
+                    "new_data": {
+                        "id": task_id,
+                        "text": message,
+                        "status": status
+                    }
+                }
+        except Exception as e:
+            return {
+                "success": False,
+                "error": str(e)
+            }
 
     def delete_task(self, task_id: int):
-        stat = self.db_client.get_all_records()
-        if task_id <= len(stat):
-            self.db_client.delete_record_by_key(key="id", value=task_id)
-            return f"Task ID{task_id} deleted!"
-        return f"Task ID{task_id} is not in task list!"
+        try:
+            stat = self.db_client.get_all_records()
+            if task_id <= len(stat):
+                self.db_client.delete_record_by_key(key="id", value=task_id)
+                return {"status": True}
+        except Exception as e:
+            return {
+                "success": False,
+                "error": str(e)
+            }
 
 
 app = FastAPI()
 
-db = DataBase()
+db = DataBase(JSONDatabase("db.json"))
 
 
 @app.get("/tasks")
@@ -51,18 +82,18 @@ def get_tasks() -> str | list:
 
 
 @app.post("/tasks")
-def create_task(text: str = Body(), status: bool = Body()) -> str:
+def create_task(text: str = Body(), status: bool = Body()) -> dict:
     result = db.create_task(text, status)
     return result
 
 
 @app.put("/tasks/{task_id}")
-def update_task(task_id: int, text: str = Body(), status: bool = Body()) -> str:
+def update_task(task_id: int, text: str = Body(), status: bool = Body()) -> dict:
     result = db.update_task(task_id, text, status)
     return result
 
 
 @app.delete("/tasks/{task_id}")
-def delete_task(task_id: int) -> str:
+def delete_task(task_id: int) -> dict:
     result = db.delete_task(task_id)
     return result
