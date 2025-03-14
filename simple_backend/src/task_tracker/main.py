c716fcdfd43d92c86c6d0095eb8c6d9810fec30a
Pestov-roman <Pestov.Roman.N@yandex.ru>
2025-03-14 17:57:24 +0500

    Organized data storage in a json file, supplemented the file readme.md

--- simple_backend/src/task_tracker/main.py
+++ simple_backend/src/task_tracker/main.py
@@ -1,41 +1,78 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from typing import Dict
+import json
+import os
 
 app = FastAPI()
+file_name = 'tasks.json'
 
 class Task(BaseModel):
     name: str
     status: str
 
     def __repr__(self):
-        return self.name
+        return f"Task(name={self.name}, status={self.status})"
 
+def read_json():
+    if not os.path.exists(file_name):
+        return {}
+    try:
+        with open(file_name, 'r', encoding='utf-8') as file:
+            return json.load(file)
+    except json.JSONDecodeError:
+        return {}
+    except Exception as err:
+        return {'error': str(err)}
 
-tasks: Dict[int, Task] = {}
+def add_json(task):
+    try:
+        info = read_json()
+        task_id = max(map(int, info.keys()), default=0) + 1
+        info[str(task_id)] = task.dict()
+        with open(file_name, 'w', encoding='utf-8') as file:
+            json.dump(info, file, indent=4, ensure_ascii=False)
+            return {'message': 'задача сохранена'}
+    except Exception as err:
+        return {'error': str(err)}
+
+def delete_json(task_id):
+    info = read_json()
+    if str(task_id) not in info:
+        raise HTTPException(status_code=404, detail='task not found')
+    del info[str(task_id)]
+    try:
+        with open(file_name, 'w', encoding='utf-8') as file:
+            json.dump(info, file, indent=4, ensure_ascii=False)
+            return {'message': 'задача удалена'}
+    except Exception as err:
+        return {'error': str(err)}
+
+def update_json(task_id, task):
+    info = read_json()
+    if str(task_id) not in info:
+        raise HTTPException(status_code=404, detail='task not found')
+    info[str(task_id)] = task.dict()
+    try:
+        with open(file_name, 'w', encoding='utf-8') as file:
+            json.dump(info, file, indent=4, ensure_ascii=False)
+            return {'message': 'задача сохранена'}
+    except Exception as err:
+        return {'error': str(err)}
 
 @app.get("/tasks")
 def get_tasks():
-    return list(tasks.values())
+    return read_json()
 
 @app.post("/tasks")
 def create_task(task: Task):
-    task_id = max(tasks, default=0) + 1
-    tasks[task_id] = task
-    return {'id': task_id, **task.dict()}
+    return add_json(task)
 
 @app.put("/tasks/{task_id}")
 def update_task(task_id: int, task: Task):
-    if task_id not in tasks:
-        raise HTTPException(status_code=404, detail='task not found')
-    tasks[task_id] = task
-    return {'id': task_id, **task.dict()}
+    return update_json(task_id, task)
 
 @app.delete("/tasks/{task_id}")
 def delete_task(task_id: int):
-    if task_id not in tasks:
-        raise HTTPException(status_code=404, detail='task not found')
-    del tasks[task_id]
-    return 'task deleted'
+    return delete_json(task_id)
 
 
